2f41c42c8ee8b89e7354a3c6bf8dfc38e5b52575
MelvinDiaz <00008821@uca.edu.sv>
2023-09-21 11:58:04 -0600

    Add: exercise 96A

--- codeforces/96A.cpp
+++ codeforces/96A.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
+    string players;
+    bool isDangerous = false;
+    cin >> players;
+    int count = 1;
+
+    for (int i = 0; i < players.size(); ++i) {
+        if (players[i] == players[i + 1])
+            ++count;
+        else
+            count = 1;
+
+        if (count == 7) {
+            isDangerous = true;
+            break;
+        }
+    }
+
+    if (isDangerous)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 
     return 0;
 }
